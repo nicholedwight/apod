08e227a85ddc1c71c91f9a32d9c6b0871df73272
Nichole Dwight <nicholedwight@gmail.com>
2020-12-03 13:01:36 -0700

    dumps data into json file, checks to make sure its not a duplicate entry for today

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import urllib.request
 import json
+import yaml
 
 from credentials import API_KEY
+from datetime import date
+
+today = date.today()
 
 
 url = 'https://api.nasa.gov/planetary/apod?'
@@ -13,4 +17,16 @@ read = urlobj.read()
 
 decode = json.loads(read.decode('utf-8'))
 
-print(decode)
+
+with open('apod-dump.json', 'r') as file:
+    data = json.loads(file.read())
+    for key, value in data.items():
+        if key == 'date':
+                if value == str(today):
+                    # Don't duplicate a day's data
+                    file.close()
+                else:
+                    # Appending new date's data to json file
+                    file = open('apod-dump.json', 'a')
+                    json.dump(decode, file, ensure_ascii=False)
+                    file.close()
